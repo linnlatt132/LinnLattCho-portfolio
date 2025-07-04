b6ed56ff88b45657239ae0c2a17769a3bec89299
linnlatt132 <clinnlatt@gmail.com>
2025-07-04 16:50:27 +0630

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+const Home = () => {
+  return (
+    <div className="flex justify-center w-full bg-blue-400 min-h-screen">
+      <div className="min-w-[700px] bg-amber-300">
+        <h1>Linn Latt Cho</h1>
+        <h1>Linn Latt Cho</h1>
+        <h1>Linn Latt Cho</h1>
+      </div>
+    </div>
+  );
+};
+export default Home;
